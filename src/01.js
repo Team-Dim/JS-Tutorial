afb494a4289281401f0f6b6247937eea62fac559
Pythonic-Rainbow <wongchingho0@gmail.com>
2025-04-18 01:51:47 +0800

    Switching to Deno

--- src/01.js
+++ src/01.js
@@ -1,3 +1,3 @@
 console.log('Try changing this sentence within the quotes!')
 console.log('In most programming languages, code are executed line by line.')
-console.log('After changing the sentence in the first line, save and re-run the code by running node src/01.js again!')
+console.log('After changing the sentence in the first line, save and re-run the code by running deno src/01.js again!')
